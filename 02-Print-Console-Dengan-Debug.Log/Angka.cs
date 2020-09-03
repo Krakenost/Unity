38a7d2845b36e60e351bdc8147f9317da30f2765
Krakenost <70700452+Krakenost@users.noreply.github.com>
2020-09-03 19:33:52 +0700

    Add files via upload

---
+++ 02-Print-Console-Dengan-Debug.Log/Angka.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Angka : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Selamat Datang Di Game Tebak Angka");
+        Debug.Log("Pilih Angka 1 Sampai 1000!");
+        Debug.Log("Nanti Angka Yang Kamu Pilih Akan Saya Tebak");
+        Debug.Log("Tekan Tombol Atas Jika Tebakan Saya Terlalu Rendah!");
+        Debug.Log("Tekan Tombol Bawah Jika Tebakan Saya Terlalu Tinggi!");
+        Debug.Log("Tekan Tombol Enter Jika Tebakan Saya Benar!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
