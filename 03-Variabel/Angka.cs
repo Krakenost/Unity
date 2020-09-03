3d5a472436e2e1fdd5a06f477b39e3c6610d36d8
Krakenost <70700452+Krakenost@users.noreply.github.com>
2020-09-03 19:44:47 +0700

    Add files via upload

---
+++ 03-Variabel/Angka.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Angka : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int Max = 1000; //integer digunakan untuk menyimpan nilai dengan tipe data decimal tanpa koma
+        float TinggiBadan = 1.7f; //float digunakan untuk menyimpan nilai decimal yang berkoma dan juga yang tidak
+        bool Hidup = true; //boolean suatu tipe data yang hanya memiliki dua kemungkinan yaitu "true/false (0/1)
+        string Nama = "Krakenost"; //string tipe data yang digunakan untuk menyinpan karakter
+        Debug.Log("Selamat Datang Di Game Tebak Angka");
+        Debug.Log("Pilih Angka 1 Sampai 1000!");
+        Debug.Log("Nanti Angka Yang Kamu Pilih Akan Saya Tebak");
+        Debug.Log("Tekan Tombol Atas Jika Tebakan Saya Terlalu Rendah!");
+        Debug.Log("Tekan Tombol Bawah Jika Tebakan Saya Terlalu Tinggi!");
+        Debug.Log("Tekan Tombol Enter Jika Tebakan Saya Benar!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
